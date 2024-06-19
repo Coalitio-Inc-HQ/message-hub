5e26394b7887254f95150c7e19baf3cf032434cd
laga-vladislav <laga.ladislav@yandex.ru>
2024-06-19 13:42:38 +0900

    Привел отступы в порядок

--- src/message/models.py
+++ src/message/models.py
@@ -1,21 +1,24 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column,relationship
-from sqlalchemy import String,Boolean,CheckConstraint,text,ForeignKey,JSON
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, Boolean, CheckConstraint, text, ForeignKey, JSON
+
 
 class Base(DeclarativeBase):
     pass
 
+
 class UserORM(Base):
-    __tablename__="user"
-    id: Mapped[int]=mapped_column(primary_key=True)
-    from_table: Mapped[int]=mapped_column(len=10)
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    from_table: Mapped[int] = mapped_column(len=10)
+
 
 class ChatORM(Base):
-    __tablename__="chat"
-    id:Mapped[int]=mapped_column(primary_key=True)
+    __tablename__ = "chat"
+    id: Mapped[int] = mapped_column(primary_key=True)
     users: Mapped[JSON]
 
+
 class MessageORM(Base):
-    __tablename__="message"
-    chat_id:Mapped[int]
-    
+    __tablename__ = "message"
+    chat_id: Mapped[int]
--- src/webclient/models.py
+++ src/webclient/models.py
@@ -1,12 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column,relationship
-from sqlalchemy import String,Boolean,CheckConstraint,text,ForeignKey,JSON
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, Boolean, CheckConstraint, text, ForeignKey, JSON
+
 
 class Base(DeclarativeBase):
     pass
 
+
 class WebUserORM(Base):
-    __tablename__="webuser"
-    login: Mapped[str]=mapped_column(len=256)
-    hash_password:Mapped[str]=mapped_column(len=1000)
-    user_id: Mapped[int]=mapped_column(primary_key=True)
+    __tablename__ = "webuser"
+    login: Mapped[str] = mapped_column(len=256)
+    hash_password: Mapped[str] = mapped_column(len=1000)
+    user_id: Mapped[int] = mapped_column(primary_key=True)
