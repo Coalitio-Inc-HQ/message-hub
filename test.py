0551fa16b8b81040f548b4331b273c50d87de558
laga-vladislav <laga.ladislav@yandex.ru>
2024-06-20 10:42:24 +0900

    причесал код связанный с ботом
    поправил импорты

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 from src.database import session_factory
 from sqlalchemy import text
 
-from src.telegram_bot.database_requests import put_to_database_telegramuser,get_telegramuser_by_user_id_from_database,get_telegramuser_by_telegram_id_from_database
-from src.message.database_requests import create_user,create_chat,add_user_to_chat
+from src.telegram_bot.database_requests import put_to_database_telegram_user, get_telegram_user_by_user_id_from_database, \
+    get_telegram_user_by_telegram_id_from_database
+from src.message.database_requests import create_user, create_chat, add_user_to_chat
 
 import asyncio
 
-async def test():
-    print(await get_telegramuser_by_telegram_id_from_database(20))
-    # print( (await create_user("tg")))
-asyncio.run(test())
 
+async def test():
+    print(await get_telegram_user_by_telegram_id_from_database(20))
+    # print( (await create_user("tg")))
+
+
+asyncio.run(test())
