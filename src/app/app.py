629450831ff4f95703e736abe6521ed32d452b28
laga-vladislav <laga.ladislav@yandex.ru>
2024-06-22 01:37:05 +0900

    Добавил PlatformRegistrationDTO, наметил lifespan

--- src/app/app.py
+++ src/app/app.py
@@ -2,11 +2,25 @@ from enum import Enum
 from fastapi import FastAPI, HTTPException
 from .database_requests import *
 
-app = FastAPI()
+from pydantic import BaseModel
+
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 class PlatformsEnum(Enum):
     telegram = "telegram"
+
+
+class PlatformRegistrationDTO(BaseModel):
+    platform_name: str
 
 
 def check_platform(func):
@@ -22,16 +36,17 @@ def check_platform(func):
 
 @app.post("/platform_registration")
 @check_platform
-async def register_platform(platform_name: str):
-    platform = await platform_registration(f"{platform_name}")
+async def register_platform(request: PlatformRegistrationDTO):
+    platform = await platform_registration(f"{request.platform_name}")
     print(platform.__dict__)
     return {"status": "ok"}
 
 
 @app.post("/create_user_from_bot")
 @check_platform
-async def create_user_by_bot(platform_name: str):
-    platform_id = await get_platform_id_by_platform_name(platform_name)
+async def create_user_by_bot(request: PlatformRegistrationDTO):  # пока что не работает
+    platform_id = await get_platform_id_by_platform_name(request.platform_name)
+    print(platform_id)
     user = await create_user_from_bot(platform_id)
     print(user.__dict__)
     return {"status": "ok"}
