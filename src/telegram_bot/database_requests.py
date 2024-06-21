e6545c786e2c214f19b07bb994a86f110f8e933b
laga-vladislav <laga.ladislav@yandex.ru>
2024-06-21 10:41:48 +0900

    telegramuser стал telegram_user
    а также исправил опечатки

--- src/telegram_bot/database_requests.py
+++ src/telegram_bot/database_requests.py
@@ -5,7 +5,7 @@ from .schemes import TelegramUserDTO
 
 async def put_to_database_telegramuser(telegram_user_id:int,user_id:int,chat_id:int)->None:
     """
-    Регестрирует пользователя в БД.
+    Регистрирует пользователя в БД.
     """
     async with session_factory() as session:
         session.add(TelegramUserORM(telegram_user_id=telegram_user_id,user_id=user_id,chat_id=chat_id))
@@ -14,7 +14,7 @@ async def put_to_database_telegramuser(telegram_user_id:int,user_id:int,chat_id:
 async def get_telegramuser_by_user_id_from_database(user_id:int)->TelegramUserDTO:
     """
     Получает пользователя из БД по user.id.
-    Возвращяет: TelegramUserDTO(telegram_user_id,user_id,chat_id).
+    Возвращает: TelegramUserDTO(telegram_user_id,user_id,chat_id).
     """
     async with session_factory() as session:
         res = await session.execute(select(TelegramUserORM).where(TelegramUserORM.user_id==user_id))
@@ -23,7 +23,7 @@ async def get_telegramuser_by_user_id_from_database(user_id:int)->TelegramUserDT
 async def get_telegramuser_by_telegram_id_from_database(telegram_user_id:int)->TelegramUserORM:
     """
     Получает пользователя из БД по telegram_user_id.
-    Возвращяет: TelegramUserDTO(telegram_user_id,user_id,chat_id).
+    Возвращает: TelegramUserDTO(telegram_user_id,user_id,chat_id).
     """
     async with session_factory() as session:
         res = await session.execute(select(TelegramUserORM).where(TelegramUserORM.telegram_user_id==telegram_user_id))
